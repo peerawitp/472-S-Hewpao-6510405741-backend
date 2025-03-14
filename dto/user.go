2ddf6ad0b0a54a83821bfa1bbcf76654e06d04f7
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2025-03-14 17:21:05 +0700

    update: add /profile route, get user profile and edit profile

--- dto/user.go
+++ dto/user.go
@@ -8,3 +8,20 @@ type CreateUserDTO struct {
 	Surname     string  `json:"surname"`
 	PhoneNumber *string `json:"phone_number"`
 }
+
+type UserProfileDTO struct {
+	ID          string  `json:"id"`
+	Email       string  `json:"email"`
+	Name        string  `json:"name"`
+	MiddleName  *string `json:"middle_name"`
+	Surname     string  `json:"surname"`
+	PhoneNumber *string `json:"phone_number"`
+	IsVerified  bool    `json:"is_verified"`
+}
+
+type EditProfileDTO struct {
+	Name        string  `json:"name" validate:"required"`
+	MiddleName  *string `json:"middle_name"`
+	Surname     string  `json:"surname" validate:"required"`
+	PhoneNumber *string `json:"phone_number"`
+}
