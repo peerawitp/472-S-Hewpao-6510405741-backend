c148bb5949c98fd0771dd3ebc26ad832f7c93903
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2025-03-25 11:42:13 +0700

    feat(verification-test): add unit test for verification usecase

--- usecase/test/mock_repos/ekyc.go
+++ usecase/test/mock_repos/ekyc.go
@@ -4,6 +4,7 @@ import (
 	"mime/multipart"
 
 	"github.com/hewpao/hewpao-backend/dto"
+	"github.com/hewpao/hewpao-backend/repository"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -14,4 +15,20 @@ type MockEKYCRepository struct {
 func (m *MockEKYCRepository) Verify(file *multipart.FileHeader) (*dto.EKYCResponseDTO, error) {
 	args := m.Called(file)
 	return args.Get(0).(*dto.EKYCResponseDTO), args.Error(1)
+}
+
+type MockEKYCRepositoryFactory struct {
+	mock.Mock
+}
+
+func (m *MockEKYCRepositoryFactory) Register(provider string, repo repository.EKYCRepository) {
+	m.Called(provider, repo)
+}
+
+func (f *MockEKYCRepositoryFactory) GetRepository(provider string) (repository.EKYCRepository, error) {
+	args := f.Called(provider)
+	if repo, ok := args.Get(0).(repository.EKYCRepository); ok {
+		return repo, args.Error(1)
+	}
+	return nil, args.Error(1)
 }
